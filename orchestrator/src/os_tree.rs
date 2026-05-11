1f741f87af6cdda22264f9fc2f9c3483698a6fdc
Jaroslav Freymann <jaroslav.freymann@fu-berlin.de>
2026-05-11 16:21:02 +0200

    feat: Add a function to update OS version
    Add a function to trigger bootc OS update. We decided to use bootc to make testing possible.
    
    Refs: #28, #29
    
    Signed-off-by: Jaroslav Freymann <jaroslav.freymann@fu-berlin.de>

--- orchestrator/src/os_tree.rs
+++ orchestrator/src/os_tree.rs
@@ -1,11 +1,12 @@
 use std::time::Duration;
+use std::process::Command;
 
 use log::info;
 use tokio::time::interval;
 
 use crate::state::{AgentState, OsState};
 
-pub fn get_inital_os_state() -> OsState {
+pub fn get_initial_os_state() -> OsState {
     OsState {
         update_pending: false,
         running_ostree_commit: String::from("current_latest"),
@@ -57,7 +58,25 @@ async fn handle_os_tree(current_state: OsState, target_state: OsState) {
     }
 }
 
+// requires root privileges
 async fn run_update_command(current_state: &OsState, target_state: &OsState) {
-    let _ = (current_state, target_state);
-    info!("Running placeholder OS update command");
+    info!("Triggering OS update to commit: {:?}", target_state.running_ostree_commit);
+
+    let status = Command::new("sudo")
+        .args(["bootc", "upgrade"])
+        .status();
+
+    match status {
+        Ok(s) if s.success() => {
+            info!("OS update staged successfully.");
+            reboot_device();
+        }
+        Ok(s) => error!("bootc upgrade failed with status: {}", s),
+        Err(e) => error!("Failed to execute bootc: {}", e),
+    }
+}
+
+fn reboot_device() {
+    info!("Rebooting system to apply OS update...");
+    let _ = Command::new("systemctl").arg("reboot").status();
 }
