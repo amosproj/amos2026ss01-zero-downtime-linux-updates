15765648029f3cb4b4e342aebe76f0a2af20c37f
Moritz Reinel <mo.ritz@fau.de>
2026-05-17 22:35:51 +0200

    feat(devcontainer): add postgres container
    Signed-off-by: Moritz Reinel <mo.ritz@fau.de>

---
+++ .devcontainer/10_setup_app_db.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+db_name="amos"
+db_user="app"
+db_password="4M0S"
+
+psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<EOF
+    CREATE USER $db_user;
+    ALTER USER $db_user PASSWORD '$db_password';
+    CREATE DATABASE $db_name;
+    GRANT ALL PRIVILEGES ON DATABASE $db_name TO $db_user;
+EOF
