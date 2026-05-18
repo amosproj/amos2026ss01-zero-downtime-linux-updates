923fafe0f9db16db04d35aeeee08f339d0dcdcfe
Moritz Reinel <mo.ritz@fau.de>
2026-05-18 18:25:16 +0200

    refactor(common): use macros for relation generation in database entities
    Co-authored-by: Jeremias Friedrich <jeremias.friedrich@fau.de>
    Signed-off-by: Moritz Reinel <mo.ritz@fau.de>

--- common/src/entities/group.rs
+++ common/src/entities/group.rs
@@ -1,29 +1,19 @@
 pub mod group {
-    // use sea_orm::entity::prelude::*;
-	use sea_orm::*;
-	// use crate::entities::Device;
+    use sea_orm::entity::prelude::*;
+	use crate::entities::Device;
 
-    #[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel, Default)]
+    #[sea_orm::model]
+    #[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel)]
     #[sea_orm(table_name = "groups")]
     pub struct Model {
         #[sea_orm(primary_key, auto_increment = true)]
         pub id: i32,
-        pub name: String,
-		// #[sea_orm(has_many)]
-    	// pub device: HasMany<crate::entities::device::device::Entity>,
+
+		pub name: String,
+
+		#[sea_orm(has_many)]
+    	pub device: HasMany<Device::Entity>,
     }
-
-    #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
-	pub enum Relation {
-		#[sea_orm(has_many = "crate::entities::device::device::Entity")]
-		Device,
-	}
-
-	impl Related<crate::entities::device::device::Entity> for Entity {
-		fn to() -> RelationDef {
-			Relation::Device.def()
-		}
-	}
 
     impl ActiveModelBehavior for ActiveModel {}
 }
