d52cdd2181b883ac42bddfe5a3e6e3da543f68a2
Moritz Reinel <mo.ritz@fau.de>
2026-05-18 15:57:25 +0200

    fix(server): add grants to dev database
    Co-authored-by: Jeremias Friedrich <jeremias.friedrich@fau.de>
    Signed-off-by: Moritz Reinel <mo.ritz@fau.de>

--- .devcontainer/10_setup_app_db.sh
+++ .devcontainer/10_setup_app_db.sh
@@ -9,5 +9,8 @@ psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<EOF
     CREATE USER $db_user;
     ALTER USER $db_user PASSWORD '$db_password';
     CREATE DATABASE $db_name;
+
+    \connect $db_name;
+    GRANT USAGE, CREATE ON SCHEMA public TO $db_user;
     GRANT ALL PRIVILEGES ON DATABASE $db_name TO $db_user;
 EOF
