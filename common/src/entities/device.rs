59eab12805352290ecf5f2ac105ea83c7b35ea36
Moritz Reinel <mo.ritz@fau.de>
2026-05-18 19:59:11 +0200

    style: fix more clippy warnings regarding module naming
    Signed-off-by: Moritz Reinel <mo.ritz@fau.de>

--- common/src/entities/device.rs
+++ common/src/entities/device.rs
@@ -1,22 +1,20 @@
-pub mod device {
-    use crate::entities::Group;
-    use sea_orm::entity::prelude::*;
+use crate::entities::Group;
+use sea_orm::entity::prelude::*;
 
-    #[sea_orm::model]
-    #[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel)]
-    #[sea_orm(table_name = "devices")]
-    pub struct Model {
-        #[sea_orm(primary_key, auto_increment = true)]
-        pub id: i32,
+#[sea_orm::model]
+#[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel)]
+#[sea_orm(table_name = "devices")]
+pub struct Model {
+    #[sea_orm(primary_key, auto_increment = true)]
+    pub id: i32,
 
-        pub uuid: String,
+    pub uuid: String,
 
-        pub hostname: String,
+    pub hostname: String,
 
-        pub group_id: Option<i32>,
-        #[sea_orm(belongs_to, from = "group_id", to = "id")]
-        pub group: HasOne<Group::Entity>,
-    }
-
-    impl ActiveModelBehavior for ActiveModel {}
+    pub group_id: Option<i32>,
+    #[sea_orm(belongs_to, from = "group_id", to = "id")]
+    pub group: HasOne<Group::Entity>,
 }
+
+impl ActiveModelBehavior for ActiveModel {}
--- common/src/entities/group.rs
+++ common/src/entities/group.rs
@@ -1,19 +1,17 @@
-pub mod group {
-    use crate::entities::Device;
-    use sea_orm::entity::prelude::*;
+use crate::entities::Device;
+use sea_orm::entity::prelude::*;
 
-    #[sea_orm::model]
-    #[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel)]
-    #[sea_orm(table_name = "groups")]
-    pub struct Model {
-        #[sea_orm(primary_key, auto_increment = true)]
-        pub id: i32,
+#[sea_orm::model]
+#[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel)]
+#[sea_orm(table_name = "groups")]
+pub struct Model {
+    #[sea_orm(primary_key, auto_increment = true)]
+    pub id: i32,
 
-        pub name: String,
+    pub name: String,
 
-        #[sea_orm(has_many)]
-        pub device: HasMany<Device::Entity>,
-    }
-
-    impl ActiveModelBehavior for ActiveModel {}
+    #[sea_orm(has_many)]
+    pub device: HasMany<Device::Entity>,
 }
+
+impl ActiveModelBehavior for ActiveModel {}
