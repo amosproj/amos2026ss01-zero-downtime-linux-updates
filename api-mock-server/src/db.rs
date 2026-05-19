3407b2615fa73ed37972cc0053a8593e34227c8a
Moritz Reinel <mo.ritz@fau.de>
2026-05-19 10:19:58 +0200

    feat(server): add db functions for group/device creation
    Co-authored-by: Jeremias Friedrich <jeremias.friedrich@fau.de>
    Signed-off-by: Moritz Reinel <mo.ritz@fau.de>

--- api-mock-server/src/db.rs
+++ api-mock-server/src/db.rs
@@ -1,18 +1,57 @@
-use sea_orm::{ConnectOptions, Database, DbErr};
+use log::debug;
+use sea_orm::{ActiveModelTrait, ConnectOptions, Database, DatabaseConnection, DbErr};
+use sea_orm::ActiveValue::{NotSet, Set};
 use sea_orm_migration::MigratorTrait;
+use std::sync::OnceLock;
 
+use amos_common::entities::{Device, Group};
 use crate::db_migration::Migrator;
+
+static DB: OnceLock<DatabaseConnection> = OnceLock::new();
+
+fn db() -> &'static DatabaseConnection {
+    DB.get().expect("Database not initialized")
+}
 
 pub async fn initalialize_db(database_url: String) -> Result<(), DbErr> {
     let mut opt = ConnectOptions::new(&database_url.to_owned());
     // SQL queries should be the last resort when debugging...
     opt.sqlx_logging_level(log::LevelFilter::Trace);
 
-    let db = Database::connect(opt).await?;
+    let conn = Database::connect(opt).await?;
 
-    db.ping().await?;
+    conn.ping().await?;
 
-    Migrator::up(&db, None).await?;
+    Migrator::up(&conn, None).await?;
+
+    DB.set(conn).map_err(|_| DbErr::Custom("DB already initialized".into()))?;
 
     Ok(())
+}
+
+pub async fn add_group(name: String) -> Result<i32, DbErr> {
+    let group = Group::ActiveModel {
+        id: NotSet,
+        name: Set(name.to_owned()),
+        // ..Default::default()
+    };
+
+    let new_group = group.insert(db()).await?;
+    debug!("Inserted group: {:?}", new_group);
+
+    return Ok(new_group.id);
+}
+
+pub async fn add_device(uuid: String, hostname: String, group_id: Option<i32>) -> Result<i32, DbErr> {
+    let device = Device::ActiveModel {
+        id: NotSet,
+        uuid: Set(uuid),
+        hostname: Set(hostname),
+        group_id: Set(group_id)
+    };
+
+    let new_device = device.insert(db()).await?;
+    debug!("Inserted device: {:?}", new_device);
+
+    return Ok(new_device.id);
 }
