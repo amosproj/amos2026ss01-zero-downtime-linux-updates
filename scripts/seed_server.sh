e6dbf9652b3dddeef3ae636dcb7649f34bea511e
Moritz Reinel <mo.ritz@fau.de>
2026-05-26 15:03:58 +0200

    chore: add seeding script for initializing the database via api with sample data
    Signed-off-by: Moritz Reinel <mo.ritz@fau.de>
    
    Signed-off-by: Jeremias Friedrich <jeremias.friedrich@fau.de>

---
+++ scripts/seed_server.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -eu
+
+readonly api_base_path="http://localhost:8080/v1"
+
+api() {
+    local path="$1"; shift
+    local method="${1:-GET}"; shift || true
+    local body="${1:-}"; shift || true
+
+    local url="${api_base_path}/${path#/}"
+    if [ -n "$body" ]; then
+        ( set -x; curl -i -sS -X "$method" "$url" -H "Content-Type: application/json" -d "$body" )
+    else
+        ( set -x; curl -i -sS -X "$method" "$url" -H "Content-Type: application/json" )
+    fi
+    echo -e "\n---------------"
+}
+
+# Create random strings:
+# uuidv4: `uuidgen`
+# sha1: `head -c 32 /dev/urandom | sha1sum`
+
+api "/tenants" POST '{ "id": 0, "name": "Weber-Lager", "description": "Meta-Ort für initialisierte unverschickte Geräte" }'
+api "/devices" POST '{ "id": 0, "uuid": "8b722f94-6852-42cf-9722-98446499a457", "hostname": "x38974", "tenant_id": 1 }'
+
+api "/os-versions" POST '{ "id": 0, "commit_hash": "092599a804d5169ae2a0a306bcb4b213b7646d28", "orchestrator_version": "0.1.0", "description": "First stable release, tested intensively" }'
+
+api "/os-assignments" POST '{ "id": 0, "os_version_id": 1, "device_id": 1 }'
+
+# api "/os-assignments?device_uuid=8b722f94-6852-42cf-9722-98446499a457" # works
+# api "/os-assignments?device_uuid=abc-123" # fails
