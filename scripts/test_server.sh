e94f2d766b6e32a23a2cc891e2313416dd28fda9
Jeremias Friedrich <jeremias.friedrich@fau.de>
2026-05-31 21:22:33 +0200

    test(mock-api-server): readd tests lost in merge and change to new format
    Signed-off-by: Jeremias Friedrich <jeremias.friedrich@fau.de>

--- scripts/test_server.sh
+++ scripts/test_server.sh
@@ -78,20 +78,23 @@ done
 # uuidv4: `uuidgen`
 # sha1: `head -c 32 /dev/urandom | sha1sum`
 
-api "/tenants" POST '{ "id": 0, "name": "Weber-Lager", "description": "Meta-Ort für initialisierte unverschickte Geräte" }' 201
-api "/devices" POST '{ "id": 0, "uuid": "8b722f94-6852-42cf-9722-98446499a457", "hostname": "x38974", "tenant_id": 1 }' 201
+api "/tenants" POST '{ "name": "Weber-Lager", "description": "Meta-Ort für initialisierte unverschickte Geräte" }' 201
+api "/tenants" POST '{ "name": "Kaufland-Fabrik-Erlangen", "description": "Stammkunde in Deutschland" }' 201
+api "/tenants" POST '{ "name": "7-Eleven-Fabrik-Tokyo", "description": "Zentrale Stelle in Chiyoda für Tokyo" }' 201
+api "/tenants" POST '{ "name": "Foodland-Fabrik-Bangkok", "description": "Hauptlagerort in Bangkok" }' 201
+api "/devices" POST '{ "uuid": "8b722f94-6852-42cf-9722-98446499a457", "hostname": "x38974", "tenant_id": 1 }' 201
 
-api "/os-versions" POST '{ "id": 0, "commit_hash": "092599a804d5169ae2a0a306bcb4b213b7646d28", "orchestrator_version": "0.1.0", "description": "First stable release, tested intensively" }' 201
+api "/os-versions" POST '{ "commit_hash": "092599a804d5169ae2a0a306bcb4b213b7646d28", "orchestrator_version": "0.1.0", "description": "First stable release, tested intensively" }' 201
 
-api "/os-assignments" POST '{ "id": 0, "os_version_id": 1, "device_id": 1 }' 201
+api "/os-assignments" POST '{ "os_version_id": 1, "device_id": 1 }' 201
 
 # api "/os-assignments?device_uuid=8b722f94-6852-42cf-9722-98446499a457" # works
 # api "/os-assignments?device_uuid=abc-123" # fails
 
 # --- reported-os-assignments ---
 
-api "/reported-os-assignments" POST '{ "id": 0, "os_version_id": 1, "device_id": 1, "updated_at": "1970-01-01T00:00:00Z" }' 201
+api "/reported-os-assignments" POST '{ "os_version_id": 1, "device_id": 1 }' 201
 
-api "/reported-os-assignments?device_uuid=8b722f94-6852-42cf-9722-98446499a457" POST '{ "id": 0, "os_version_id": 1, "device_id": 0, "updated_at": "1970-01-01T00:00:00Z" }' 201
+api "/reported-os-assignments?device_uuid=8b722f94-6852-42cf-9722-98446499a457" POST '{ "os_version_id": 1 }' 201
 
-api "/reported-os-assignments?device_uuid=00000000-0000-0000-0000-000000000000" POST '{ "id": 0, "os_version_id": 1, "device_id": 0, "updated_at": "1970-01-01T00:00:00Z" }' 404
+api "/reported-os-assignments?device_uuid=00000000-0000-0000-0000-000000000000" POST '{ "os_version_id": 1 }' 404
