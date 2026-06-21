8e6c89771b373d8e137aedd8e88d4f4195c56270
Moritz Reinel <mo.ritz@fau.de>
2026-06-21 19:07:32 +0200

    chore: remove legacy mock script
    Signed-off-by: Moritz Reinel <mo.ritz@fau.de>

--- scripts/bootc_dummy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# This script serves as a mock for the real bootc binary. Can be used for local development where the real bootc behaviour isn't needed at the moment.
-# For example, mounting into the bootc build: `podman run --rm -it -v "./scripts/bootc_dummy.sh:/usr/local/bin/bootc" amos:latest`
-
-cat <<EOF
-{"apiVersion":"org.containers.bootc/v1","kind":"BootcHost","metadata":{"name":"host"},"spec":{"bootOrder":"default","image":null},"status":{"booted":{"ostree":{"checksum":"123456"},"image":null},"rollback":null,"rollbackQueued":false,"staged":null,"type":null,"usrOverlay":null}}
-EOF
