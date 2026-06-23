5daef554c54d862993ae7bafb6034625daeefdfd
patridu <83885728+patridu@users.noreply.github.com>
2026-06-23 18:43:13 +0000

    fix(orchestrator): Minor fixes
    Signed-off-by: patridu <83885728+patridu@users.noreply.github.com>
    Co-authored-by: Jeremias Friedrich <jeremias.friedrich@fau.de>
    
    Signed-off-by: Jeremias Friedrich <jeremias.friedrich@fau.de>

--- orchestrator/src/util/device_jwt.rs
+++ orchestrator/src/util/device_jwt.rs
@@ -23,8 +23,8 @@ impl DeviceJwtProvider {
         }
     }
 
-    pub fn token<'a>(&'a mut self, device_uuid: &str) -> anyhow::Result<&'a str> {
-        if SystemTime::now() > self.expire_time - REFRESH_BEFORE {
+    pub fn token(&mut self, device_uuid: &str) -> anyhow::Result<&str> {
+        if SystemTime::now() + REFRESH_BEFORE > self.expire_time {
             debug!("Refreshing device JWT token");
 
             self.expire_time = SystemTime::now() + Duration::from_secs(MAX_TOKEN_LIFETIME as u64);
