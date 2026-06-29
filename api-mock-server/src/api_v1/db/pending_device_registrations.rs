6d4f0aa8dc9ab45239436d17842e463e59e61146
Moritz Reinel <mo.ritz@fau.de>
2026-06-30 01:09:09 +0200

    feat(api): add device registration logic
    Signed-off-by: Moritz Reinel <mo.ritz@fau.de>

--- api-mock-server/src/api_v1/db/pending_device_registrations.rs
+++ api-mock-server/src/api_v1/db/pending_device_registrations.rs
@@ -2,7 +2,7 @@ use crate::dtos;
 use amos_common::entities::PendingDeviceRegistration;
 use log::debug;
 use sea_orm::ActiveValue::{NotSet, Set};
-use sea_orm::{ActiveModelTrait, DbErr, EntityTrait, PaginatorTrait, QueryOrder};
+use sea_orm::{ActiveModelTrait, ColumnTrait, DbErr, EntityTrait, PaginatorTrait, QueryFilter, QueryOrder};
 
 use super::db;
 
@@ -50,4 +50,18 @@ pub async fn delete_pending_device_registration(id: i32) -> Result<u64, DbErr> {
         .exec(&db)
         .await?;
     Ok(del.rows_affected)
+}
+
+pub async fn search_pending_device_registration(
+    serial_number: String,
+    endorsement_public_key: String,
+) -> Result<Option<dtos::PendingDeviceRegistration::Model>, DbErr> {
+    let db = db!();
+    let result = dtos::PendingDeviceRegistration::Entity::find()
+        .filter(dtos::PendingDeviceRegistration::Column::SerialNumber.eq(serial_number))
+        .filter(dtos::PendingDeviceRegistration::Column::EndorsementPublicKey.eq(endorsement_public_key))
+        .one(&db)
+        .await?;
+
+    Ok(result)
 }
