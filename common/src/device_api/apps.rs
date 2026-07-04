f94e8e935915d01488f0d94da304f93e2d0880a4
patridu <83885728+patridu@users.noreply.github.com>
2026-07-04 14:30:18 +0000

    chore: Adding device API types according to spec
    Signed-off-by: patridu <83885728+patridu@users.noreply.github.com>

---
+++ common/src/device_api/apps.rs
@@ -0,0 +1,22 @@
+/// GET /device/apps - Get the assigned applications
+pub type GetResponse = Vec<GetResponseItem>;
+
+#[derive(Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub struct GetResponseItem {
+    pub id: i32,
+    pub application_id: i32,
+    pub image: String,
+    pub version: i32,
+    pub config_version: i32,
+    pub config: Option<crate::entities::ContainerConfigV1>,
+    pub deleted_at: Option<std::time::SystemTime>,
+    pub superseded_by: Option<i32>,
+}
+
+/// PUT /device/apps - Report the currently running applications
+pub type PutBody = Vec<PutBodyItem>;
+
+#[derive(Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub struct PutBodyItem {
+    pub application_config_id: i32,
+}
---
+++ common/src/device_api/os.rs
@@ -0,0 +1,16 @@
+/// GET /device/os - Get the assigned OS version
+#[derive(Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub struct GetResponse {
+    pub id: i32,
+    pub commit_hash: String,
+    pub orchestrator_version: String,
+    pub description: Option<String>,
+    pub deleted_at: Option<std::time::SystemTime>,
+    pub superseded_by: Option<i32>,
+}
+
+/// PUT /device/os - Report the currently running OS version
+#[derive(Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub struct PutBody {
+    pub os_version_id: i32,
+}
