3e4bd10a3792c436a453441a80f0ab3f0e8050b8
patridu <83885728+patridu@users.noreply.github.com>
2026-07-11 16:52:13 +0000

    fmt: clippy and format
    Signed-off-by: patridu <83885728+patridu@users.noreply.github.com>

--- orchestrator/src/application.rs
+++ orchestrator/src/application.rs
@@ -1,7 +1,6 @@
 //! Wrap Podman containers, then manage their lifecycle and logs
 
 use std::{sync::Arc, time::Duration};
-use tracing::{debug, error, info, warn};
 
 use amos_common::entities::ContainerConfigV1;
 
