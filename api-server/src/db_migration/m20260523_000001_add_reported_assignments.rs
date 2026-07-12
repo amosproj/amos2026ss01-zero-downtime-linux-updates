7ba7b0d72100171d22b7cd743ee102d84be5038b
Beambamacrys <julian-schuetz@web.de>
2026-07-13 00:39:25 +0200

    fix(api): log msg and move unique to old migration
    Signed-off-by: Beambamacrys <julian-schuetz@web.de>

--- api-server/src/db_migration/m20260523_000001_add_reported_assignments.rs
+++ api-server/src/db_migration/m20260523_000001_add_reported_assignments.rs
@@ -21,6 +21,30 @@ impl MigrationTrait for Migration {
         .await?;
         create_table(manager, &schema, dtos::ReportedOsAssignment::Entity).await?;
 
+        manager
+            .create_index(
+                Index::create()
+                    .name("idx_reported_app_assignment_device_config_unique")
+                    .table(dtos::ReportedApplicationAssignment::Entity)
+                    .col(dtos::ReportedApplicationAssignment::Column::DeviceId)
+                    .col(dtos::ReportedApplicationAssignment::Column::ApplicationConfigId)
+                    .unique()
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .create_index(
+                Index::create()
+                    .name("idx_reported_os_assignment_device_osversion_unique")
+                    .table(dtos::ReportedOsAssignment::Entity)
+                    .col(dtos::ReportedOsAssignment::Column::DeviceId)
+                    .col(dtos::ReportedOsAssignment::Column::OsVersionId)
+                    .unique()
+                    .to_owned(),
+            )
+            .await?;
+
         Ok(())
     }
 
